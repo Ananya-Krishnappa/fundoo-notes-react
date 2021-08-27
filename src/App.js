651fd03cb8748c9a24aff62fc52c1b38e3fc0faf
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-08-27 22:25:23 +0530

    feat:basics of react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "Ananya",
+      noteList: [
+        {
+          title: "note1",
+          description: "note desc1",
+          isPinned: false,
+        },
+        {
+          title: "note2",
+          description: "note desc2",
+          isPinned: false,
+        },
+        {
+          title: "note3",
+          description: "note desc3",
+          isPinned: false,
+        },
+      ],
+      newNoteTitle: "",
+      newNoteDescription: "",
+    };
+  }
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Ananya" ? "Anu" : "Ananya",
+    });
+  };
+  updateNewNoteTitle = (event) => {
+    this.setState({
+      newNoteTitle: event.target.value,
+    });
+  };
+  updateNewNoteDescription = (event) => {
+    this.setState({
+      newNoteDescription: event.target.value,
+    });
+  };
+  createNote = () => {
+    if (
+      !this.state.noteList.find((note) => {
+        return note.title === this.state.newNoteTitle;
+      })
+    ) {
+      this.setState({
+        noteList: [
+          ...this.state.noteList,
+          {
+            title: this.state.newNoteTitle,
+            description: this.state.newNoteDescription,
+            isPinned: false,
+          },
+        ],
+      });
+    }
+  };
+  populateNotes = () => {
+    return this.state.noteList.map((note) => (
+      <article className="card">
+        <div className="text">
+          <h3>{note.title}</h3>
+          <p>{note.description}</p>
+        </div>
+      </article>
+    ));
+  };
+  render = () => {
+    return (
+      <div>
+        <h4 className="bg-primary text-white text-center p-2">
+          {this.state.userName}'s Fundoo Notes
+        </h4>
+        <div className="container-fluid">
+          <div className="my-1">
+            <input
+              className="form-control"
+              value={this.state.newNoteTitle}
+              onChange={this.updateNewNoteTitle}
+            ></input>
+            <input
+              className="form-control"
+              value={this.state.newNoteDescription}
+              onChange={this.updateNewNoteDescription}
+            ></input>
+            <button className="btn btn-primary m-2" onClick={this.createNote}>
+              Add Note
+            </button>
+          </div>
+          <main className="cards">{this.populateNotes()}</main>
+        </div>
+      </div>
+    );
+  };
 }
-
-export default App;
