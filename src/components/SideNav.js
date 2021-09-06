f51def785429145add358e24a82ea1905882a426
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-09-07 01:01:21 +0530

    feat:update ui for note

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
---
+++ src/components/updateNote/UpdateNote.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
+import { ClickAwayListener } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+const styles = (theme) => ({
+    root: {
+        margin: 0,
+        padding: theme.spacing(2),
+    },
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
+    },
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+    const { children, classes, onClose, ...other } = props;
+    return (
+        <MuiDialogTitle disableTypography className={classes.root} {...other}>
+            <Typography variant="h6">{children}</Typography>
+            {onClose ? (
+                <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+                    <CloseIcon />
+                </IconButton>
+            ) : null}
+        </MuiDialogTitle>
+    );
+});
+
+const DialogContent = withStyles((theme) => ({
+    root: {
+        padding: theme.spacing(2),
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '100%',
+        },
+    },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+    root: {
+        margin: 0,
+        padding: theme.spacing(1),
+    },
+}))(MuiDialogActions);
+
+export default function UpdateNote(props) {
+    const [note, setNote] = useState(props.note);
+    const handleClose = () => {
+        props.handleCloseCallback();
+    };
+    useEffect(() => {
+        setNote(props.note);
+    });
+    const handleClickAway = () => {
+        console.log("click away", note);
+    }
+    const syncTitle = (title) => {
+        setNote({ ...note, title });
+    }
+    const syncDescription = (description) => {
+        setNote({ ...note, description });
+    }
+    const syncIsPinned = () => {
+        setNote({ ...note, isPinned: !note.isPinned });
+    }
+    return (
+        <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={props.isModalOpen}>
+            <ClickAwayListener onClickAway={handleClickAway}>
+                <div>
+                    <DialogContent dividers>
+                        <div className="create-note-container">
+                            <React.Fragment><div className={note.isPinned ? "pin-note pin" : "pin-note"}
+                                onClick={syncIsPinned}></div>
+                                <TextField autoComplete="off" id="title-input" placeholder="Title" fullWidth
+                                    onChange={(event) => syncTitle(event.target.value)} name="title" value={note.title}
+                                    InputProps={{ disableUnderline: true }} /></React.Fragment>
+                            <TextField autoComplete="off" id="desc-input" placeholder="Take a note..." fullWidth
+                                onChange={(event) => syncDescription(event.target.value)}
+                                name="description" value={note.description}
+                                InputProps={{ disableUnderline: true }} />
+                        </div>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button autoFocus onClick={handleClose} color="primary">
+                            Close
+                        </Button>
+                    </DialogActions>
+                </div>
+            </ClickAwayListener >
+        </Dialog>
+    );
+}
