c346b03120ae7fe32021032b36ff0b3a17ba2331
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-09-08 00:12:03 +0530

    feat:display archive,trash,pin notes on hover and perform actions

--- src/components/card/CardList.js
+++ src/components/card/CardList.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -13,6 +13,10 @@ import Notification from "../Notification";
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import ArchiveOutlinedIcon from '@material-ui/icons/ArchiveOutlined';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 const useStyles = makeStyles({
     root: {
         minWidth: 275,
@@ -33,13 +37,24 @@ const useStyles = makeStyles({
 export default function CardList(props) {
     const { userId } = useContext(AuthContext);
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [noteToDelete, setNoteToDelete] = useState({});
+    const [open, setOpen] = useState(false);
+    const [noteToUpdate, setNoteToUpdate] = useState({});
     const [notify, setNotify] = useState({
         isOpen: false,
         message: "",
         type: "",
     });
-    const [open, setOpen] = useState(false);
-    const [noteToUpdate, setNoteToUpdate] = useState({});
+    const handleMenuClick = (event, note) => {
+        setAnchorEl(event.currentTarget);
+        event.stopPropagation();
+        setNoteToDelete(note);
+    };
+    const handleMenuClose = (event) => {
+        setAnchorEl(null);
+        event.stopPropagation();
+    };
     const handleClickOpen = (event, updateNote) => {
         setOpen(true);
         setNoteToUpdate(updateNote);
@@ -72,9 +87,11 @@ export default function CardList(props) {
         });
     }
     const showActionPanel = (id) => {
+        document.getElementById("pin" + id).classList.remove("display-card-action");
         document.getElementById(id).classList.remove("display-card-action");
     }
     const hideActionPanel = (id) => {
+        document.getElementById("pin" + id).classList.add("display-card-action");
         document.getElementById(id).classList.add("display-card-action");
     }
     const archiveNoteFunc = (event, note) => {
@@ -83,6 +100,19 @@ export default function CardList(props) {
             userId, isArchived: true
         };
         archiveNote(note._id, reqBody).then((res) => {
+            updateNotesSuccess(res);
+        }).catch((error) => {
+            updateNotesError(error);
+        });
+    }
+    const handleDeleteNote = (event) => {
+        console.log(noteToDelete);
+        setAnchorEl(null);
+        event.stopPropagation();
+        const reqBody = {
+            userId, isTrashed: true
+        };
+        trashNote(noteToDelete._id, reqBody).then((res) => {
             updateNotesSuccess(res);
         }).catch((error) => {
             updateNotesError(error);
@@ -111,7 +141,9 @@ export default function CardList(props) {
                                         <Card onMouseOut={() => hideActionPanel(note._id)} onMouseOver={() => showActionPanel(note._id)} className={clsx(classes.root, "note-card")}
                                             onClick={(event) => handleClickOpen(event, note)}>
                                             <CardContent>
-                                                <Typography onClick={(event) => pinNoteFunc(event, note)} className={note.isPinned ? "pin-note pin" : "pin-note"}></Typography>
+                                                <Typography onClick={(event) => pinNoteFunc(event, note)} id={"pin" + note._id} className="display-card-action">
+                                                    <div className={note.isPinned ? "pin-note pin" : "pin-note"}></div>
+                                                </Typography>
                                                 <Typography className={classes.title} color="textSecondary" gutterBottom>
                                                     {note.title}
                                                 </Typography>
@@ -121,6 +153,23 @@ export default function CardList(props) {
                                             </CardContent>
                                             <CardActions id={note._id} className="card-action-panel display-card-action">
                                                 <Button size="small" onClick={(event) => archiveNoteFunc(event, note)}><ArchiveOutlinedIcon></ArchiveOutlinedIcon></Button>
+                                                <IconButton
+                                                    aria-label="more"
+                                                    aria-controls="long-menu"
+                                                    aria-haspopup="true" onClick={(event) => handleMenuClick(event, note)}
+                                                >
+                                                    <MoreVertIcon />
+                                                </IconButton>
+                                                <Menu
+                                                    id="simple-menu"
+                                                    anchorEl={anchorEl}
+                                                    keepMounted
+                                                    open={Boolean(anchorEl)}
+                                                    onClose={handleMenuClose} className="menu-list"
+                                                >
+                                                    <MenuItem onClick={(event) => handleDeleteNote(event)}>Delete note</MenuItem>
+                                                    <MenuItem onClick={(event) => handleMenuClose(event)}>Add label</MenuItem>
+                                                </Menu>
                                             </CardActions>
                                         </Card>
                                     </Grid>
