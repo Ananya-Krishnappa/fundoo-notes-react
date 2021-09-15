582a42de24dd36716213741c434ca953c3e04239
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-09-15 23:00:58 +0530

    feat:added token in headers for authentication

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 axios.defaults.baseURL = process.env.React_App_BASEURL;
-
+const getHeader = () => {
+    return {
+        headers: {
+            Authorization: `Bearer<${localStorage.getItem("webToken")}>`
+        }
+    }
+}
 export const register = (userData) => {
     return axios.post("/register", userData);
 }
@@ -11,40 +17,40 @@ export const forgotPassword = (userData) => {
     return axios.post("/forgotPassword", userData);
 }
 export const findAllNotes = (userId) => {
-    return axios.post("/findNotes/all", { userId: userId });
+    return axios.post("/findNotes/all", { userId: userId }, getHeader());
 }
 export const findNotesByLabelName = (userId, labelName) => {
-    return axios.post(`/findNotesByLabelName/${labelName}`, { userId: userId });
+    return axios.post(`/findNotesByLabelName/${labelName}`, { userId: userId }, getHeader());
 }
 export const findTrashedNotes = (userId) => {
-    return axios.post("/findNotes/trash", { userId: userId });
+    return axios.post("/findNotes/trash", { userId: userId }, getHeader());
 }
 export const findArchivedNotes = (userId) => {
-    return axios.post("/findNotes/archive", { userId: userId });
+    return axios.post("/findNotes/archive", { userId: userId }, getHeader());
 }
 export const createNote = (noteData) => {
-    return axios.post("/notes", noteData);
+    return axios.post("/notes", noteData, getHeader());
 }
 export const updateNote = (noteId, noteData) => {
-    return axios.put(`/notes/${noteId}`, noteData);
+    return axios.put(`/notes/${noteId}`, noteData, getHeader());
 }
 export const pinNote = (noteId, noteData) => {
-    return axios.put(`/pinNote/${noteId}`, noteData);
+    return axios.put(`/pinNote/${noteId}`, noteData, getHeader());
 }
 export const archiveNote = (noteId, noteData) => {
-    return axios.put(`/archiveNote/${noteId}`, noteData);
+    return axios.put(`/archiveNote/${noteId}`, noteData, getHeader());
 }
 export const trashNote = (noteId, noteData) => {
-    return axios.put(`/trashNote/${noteId}`, noteData);
+    return axios.put(`/trashNote/${noteId}`, noteData, getHeader());
 }
 export const deleteNoteForever = (noteId, noteData) => {
-    return axios.delete(`/notes/${noteId}`, noteData);
+    return axios.delete(`/notes/${noteId}`, noteData, getHeader());
 }
 export const createLabel = (labelData) => {
-    return axios.post("/label", labelData);
+    return axios.post("/label", labelData, getHeader());
 }
 export const findAllLabel = () => {
-    return axios.get("/label");
+    return axios.get("/label", getHeader());
 }
 
 
